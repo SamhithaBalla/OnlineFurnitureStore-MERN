262b85fbcb0f306178eba08ebe586417f7565cfa
SamhithaBalla <58493611+SamhithaBalla@users.noreply.github.com>
2020-05-16 12:32:04 -0500

    Add files via upload

---
+++ ecommerce/models/order.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
+
+const CartItemSchema = new mongoose.Schema(
+  {
+    product: { type: ObjectId, ref: "Product" },
+    name: String,
+    price: Number,
+    count: Number
+  },
+  { timestamps: true }
+);
+
+const CartItem = mongoose.model("CartItem", CartItemSchema);
+
+const OrderSchema = new mongoose.Schema(
+  {
+    products: [CartItemSchema],
+    user: { type: ObjectId, ref: "User" }
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = { Order, CartItem };
