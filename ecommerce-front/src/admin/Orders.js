a3bca1d10857e55dbc25a21880521c1349ff314b
SamhithaBalla <58493611+SamhithaBalla@users.noreply.github.com>
2020-05-16 12:47:00 -0500

    Add files via upload

---
+++ ecommerce-front/src/admin/Orders.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
+import { listOrders } from "./apiAdmin";
+import moment from "moment";
+
+const Orders = () => {
+    const [orders, setOrders] = useState([]);
+    const [statusValues, setStatusValues] = useState([]);
+
+    const { user, token } = isAuthenticated();
+
+    const loadOrders = () => {
+        listOrders(user._id, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setOrders(data);
+            }
+        });
+    };
+
+    
+
+    const showOrdersLength = () => {
+        if (orders.length > 0) {
+            return (
+                <h1 className="text-danger display-2">
+                    Total orders: {orders.length}
+                </h1>
+            );
+        } else {
+            return <h1 className="text-danger">No orders</h1>;
+        }
+    };
+
+    const showInput = (key, value) => (
+        <div className="input-group mb-2 mr-sm-2">
+            <div className="input-group-prepend">
+                <div className="input-group-text">{key}</div>
+            </div>
+            <input
+                type="text"
+                value={value}
+                className="form-control"
+                readOnly
+            />
+        </div>
+    );
+
+
+    return (
+        <Layout
+            title="Orders"
+            description={`G'day ${
+                user.name
+            }, you can manage all the orders here`}
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    {showOrdersLength()}
+
+                    {orders.map((o, oIndex) => {
+                        return (
+                            <div
+                                className="mt-5"
+                                key={oIndex}
+                                style={{ borderBottom: "5px solid indigo" }}
+                            >
+                                <h2 className="mb-5">
+                                    <span className="bg-primary">
+                                        Order ID: {o._id}
+                                    </span>
+                                </h2>
+
+
+                                <h3 className="mt-4 mb-4 font-italic">
+                                    Total products in the order:{" "}
+                                    {o.products.length}
+                                </h3>
+
+                                {o.products.map((p, pIndex) => (
+                                    <div
+                                        className="mb-4"
+                                        key={pIndex}
+                                        style={{
+                                            padding: "20px",
+                                            border: "1px solid indigo"
+                                        }}
+                                    >
+                                        {showInput("Product name", p.name)}
+                                        {showInput("Product price", p.price)}
+                                        {showInput("Product total", p.count)}
+                                        {showInput("Product Id", p._id)}
+                                    </div>
+                                ))}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default Orders;
